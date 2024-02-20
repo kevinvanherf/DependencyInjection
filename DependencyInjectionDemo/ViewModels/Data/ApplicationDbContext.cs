d0369c89efa48bb6608ac5ffeccecb95d658a569
kevinvanherf <kevin.vanherf@gmail.com>
2024-02-20 09:30:30 +0100

    initial dependensie injection porject

---
+++ DependencyInjectionDemo/ViewModels/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DependencyInjectionDemo.ViewModels.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DependencyInjectionDemo.ViewModels.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; } = default!;
+    }
+}
---
+++ DependencyInjectionDemo/ViewModels/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DependencyInjectionDemo.ViewModels.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public decimal? Stock { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal? Price { get; set; }
+    }
+}
